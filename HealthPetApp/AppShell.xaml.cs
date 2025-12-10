bb0ac3951b2f5affe403588a2bd7e0bea8069c20
bruno <bruno.portes@onfriday.dev>
2025-12-09 21:25:48 -0300

    Add project files.

---
+++ HealthPetApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthPetApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HealthPetApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthPetApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
