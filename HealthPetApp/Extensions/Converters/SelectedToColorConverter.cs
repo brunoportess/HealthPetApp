22fb17572e7fed3fb60f52f7d254fcd8b6418a89
bruno <bruno.portes@onfriday.dev>
2025-12-10 19:03:43 -0300

    background color converter

---
+++ HealthPetApp/Extensions/Converters/SelectedToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace HealthPetApp.Extensions.Converters
+{
+    public class SelectedToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isSelected)
+            {
+                // Retorna uma cor de destaque se selecionado, senão uma cor neutra
+                return isSelected ? Color.FromArgb("#F5B7B1") : Color.FromArgb("#EDEDED");
+            }
+            return Color.FromArgb("#EDEDED");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
