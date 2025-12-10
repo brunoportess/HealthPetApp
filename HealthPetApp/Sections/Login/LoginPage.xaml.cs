3e0b04859a575827fd4bb10db92dc26086b18db3
bruno <bruno.portes@onfriday.dev>
2025-12-10 19:06:28 -0300

    login page

---
+++ HealthPetApp/Sections/Login/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AptiJobs.Sections.Login;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HealthPetApp/Sections/Login/LoginPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using HealthPetApp;
+using HealthPetApp.Sections.Home;
+
+namespace AptiJobs.Sections.Login
+{
+    internal partial class LoginPageViewModel : BasePageViewModel
+    {
+        public LoginPageViewModel()
+        {
+            
+        }
+
+        [RelayCommand]
+        public async Task Login()
+        {
+            await Navigation.Navigate<HomePageViewModel>();
+        }
+        
+    }
+}
