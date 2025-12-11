8c9b99443c6a1dbba141948b5f7242f0a27dcbcf
bruno <bruno.portes@onfriday.dev>
2025-12-10 23:23:46 -0300

    convert border color

---
+++ HealthPetApp/Extensions/Converters/SelectedToStrokeBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace HealthPetApp.Extensions.Converters
+{
+    public class SelectedToStrokeBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = (bool)value;
+
+            return isSelected
+                ? new SolidColorBrush(Color.FromArgb("#FF7F00")) // cor do stroke quando selecionado
+                : new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
